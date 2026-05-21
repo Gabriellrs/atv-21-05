e21d0655ff5e2272ab660840f84c8452143337e7
Gabriel <gabrielrsilva1609@gmail.com>
2026-05-21 19:54:45 -0300

    feat: adicionar operações matemáticas básicas

---
+++ index.js
@@ -0,0 +1,11 @@
+// Arquivo principal com código simples e funcional
+function somarNumeros(a, b) {
+    return a + b;
+}
+
+function multiplicarNumeros(a, b) {
+    return a * b;
+}
+
+console.log("Soma: ", somarNumeros(5, 5));
+console.log("Multiplicação: ", multiplicarNumeros(5, 5));
